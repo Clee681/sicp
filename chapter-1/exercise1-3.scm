486986c49a9ff19b2771e2eaf6def810611b051f
Clee681 <christopher.lee681@gmail.com>
2015-02-17 22:04:54 -0500

    Extract functions

--- chapter-1/exercise1-3.scm
+++ chapter-1/exercise1-3.scm
@@ -1,9 +1,13 @@
 ; exercise 1.3
 
+(define (square x) (* x x))
+
+(define (sum-of-squares x y) (+ (square x) (square y)))
+
 (define (sum-squares-of-largest-2 a b c)
-  (cond ((and (<= a b) (<= a c)) (+ (* b b) (* c c)))
-	((and (<= b a) (<= b c)) (+ (* a a) (* c c)))
-	(else (+ (* a a) (* b b)))))
+  (cond ((and (<= a b) (<= a c)) (sum-of-squares b c))
+	((and (<= b a) (<= b c)) (sum-of-squares a c))
+	(else (sum-of-squares a b))))
 
 ; tests
 
